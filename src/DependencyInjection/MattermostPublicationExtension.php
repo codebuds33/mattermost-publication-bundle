143c74327803d5d819936c54bfb19e001b84a9bf
Dennis de Best <dennis@debest.fr>
2020-04-05 14:39:36 +0200

    Initial commit

---
+++ src/DependencyInjection/MattermostPublicationExtension.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace CodeBuds\MattermostPublicationBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+
+class MattermostPublicationExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        try {
+            $loader->load('services.xml');
+        } catch (\Exception $exception){
+            var_dump($exception);
+        }
+
+        $configuration = $this->getConfiguration($configs, $container);
+
+        $config = $this->processConfiguration($configuration, $configs);
+
+        $definition = $container->getDefinition("codebuds_mattermost_publication.mattermost_publication");
+        $definition->setArgument(0, $config['webhook_url']);
+    }
+}
---
+++ src/MattermostPublicationBundle.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace CodeBuds\MattermostPublicationBundle;
+
+use CodeBuds\MattermostPublicationBundle\DependencyInjection\MattermostPublicationExtension;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MattermostPublicationBundle extends Bundle
+{
+        public function getContainerExtension()
+        {
+            if (null === $this->extension){
+                $this->extension = new MattermostPublicationExtension();
+            }
+
+            return $this->extension;
+        }
+}
